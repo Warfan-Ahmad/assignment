4d705e2c0c6a396dc9ace05afc12d82e5a5992dc
Warfan-Ahmad <warfanahmad@gmail.com>
2021-11-11 10:27:19 +0530

    second push

--- shape.js
+++ shape.js
@@ -1,18 +1,19 @@
 "use strict"
 
 class shape{
+    constructor(color)
+    {
+        this.color = color;
+    }
     drawShape()
     {
         const a = 10;
     }
     calculateArea()
     {
-
-    }
-    color(){
-        const color = 'blue';
     }
 }
 
+const shape1 = new shape('blue');
 
 export {shape}
