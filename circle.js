a56e86e4622774d0373cbd75f64b3a73688edd21
Warfan-Ahmad <warfanahmad@gmail.com>
2021-11-11 10:21:44 +0530

    first push

---
+++ circle.js
@@ -0,0 +1,12 @@
+"use strict"
+
+import { shape } from "./shape"
+
+class Circle extends shape{
+    calculateArea()
+    {
+        const area = 10;
+    }
+}
+
+const newCircle = new Circle();
---
+++ shape.js
@@ -0,0 +1,18 @@
+"use strict"
+
+class shape{
+    drawShape()
+    {
+        const a = 10;
+    }
+    calculateArea()
+    {
+
+    }
+    color(){
+        const color = 'blue';
+    }
+}
+
+
+export {shape}
